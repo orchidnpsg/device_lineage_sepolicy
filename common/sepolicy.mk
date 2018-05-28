AospExtended/platform_device_aosp_sepolicy

sepolicy: Move aosp policy to platform
Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

common/sepolicy.mk
@@ -1,7 +1,10 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from Aosp
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/aosp/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/aosp/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aosp/sepolicy/common/private

orchidnpsg/device_lineage_sepolicy

common/sepolicy.mk
@@ -1,7 +1,10 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from aim
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/aim/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/aim/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aim/sepolicy/common/private
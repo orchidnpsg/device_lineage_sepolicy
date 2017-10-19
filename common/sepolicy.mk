ad634f736ea3a82504b2ff7c4e729ae160d5e3d0
Ethan Chen <intervigil@gmail.com>
2017-10-18 19:11:22 -0700

    sepolicy: Move aosp policy to platform
    Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,7 +1,10 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from aim
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/aim/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/aim/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aim/sepolicy/common/private
