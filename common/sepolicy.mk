AospExtended/platform_device_aosp_sepolicy

sepolicy: common: add rules for aosp vibrator hal
Change-Id: I1e8aba0e0ce1a51cfcd54c734d58f10f0613f773

common/sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aosp/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/aosp/sepolicy/common/vendor

orchidnpsg/device_lineage_sepolicy

common/sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aim/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/aim/sepolicy/common/vendor